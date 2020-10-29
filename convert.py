1a97749a9ed2d886beb87ee2fa13695bf2e908d3
luhao007 <luhao007@gmail.com>
2020-10-29 19:54:51 +0900

    Add dtd, fix the case

--- convert.py
+++ convert.py
@@ -21,7 +21,7 @@ def convert(folder):
         if not file.endswith('.txt'):
             continue
 
-        level = SubElement(collection, 'Level', {'id': str(i)})
+        level = SubElement(collection, 'Level', {'Id': str(i)})
 
         with open('/'.join([folder, file]), 'r') as f:
             for r in f.readlines():
@@ -30,9 +30,18 @@ def convert(folder):
 
         i += 1
 
-    s = ElementTree.tostring(root, encoding='utf-8', xml_declaration=False)
+    s = ElementTree.tostring(root, encoding='utf-8')
+    s = b'<!DOCTYPE SokobanLevels SYSTEM "SokobanLev.dtd">' + s
     parsed = minidom.parseString(s)
     xml = parsed.toprettyxml(indent='  ', encoding='utf-8', newl='\n')
 
     with open(f'{folder}.slc', 'wb') as f:
         f.write(xml)
+
+
+def main():
+    convert('levels/Sokoban Perfect')
+
+
+if __name__ == '__main__':
+    main()
